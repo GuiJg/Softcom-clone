7f39a66b79bb2b22af85c86ac0d3ee429c6296ff
GuiJg <j.gu1lh3rm325@gmail.com>
2023-05-12 08:08:54 -0300

    fourth

--- script.js
+++ script.js
@@ -11,10 +11,29 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-(function () {
-    var menu = document.getElementById('header-softcom'); // colocar em cache
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) menu.classList.add('header-scroll'); // > 0 ou outro valor desejado
-        else menu.classList.remove('header-scroll');
-    });
-})();
+  const menu = document.getElementById("header-softcom"); // colocar em cache
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      menu.classList.add("header-scroll");
+      for (let i = 0; i < document.querySelectorAll(".header-link").length; i++) {
+        document.querySelectorAll(".header-link")[i].style.color = "#fff"
+      }
+      document.querySelector(".header-fale-conosco").style.color = "#fff"
+      document.querySelectorAll(".header-proposta")[0].style.color = "#fff"
+      document.querySelectorAll(".header-proposta")[1].style.color = "#fff"
+      document.querySelectorAll(".mensage-icon")[0].style.filter = "none"
+      document.querySelectorAll(".mensage-icon")[1].style.filter = "none"
+      document.querySelector(".softcom-logo").src = "assets/softcom-yellow.png"
+    } else {
+      menu.classList.remove("header-scroll");
+      for (let i = 0; i < document.querySelectorAll(".header-link").length; i++) {
+        document.querySelectorAll(".header-link")[i].style.color = "#000"
+      }
+      document.querySelector(".header-fale-conosco").style.color = "#000"
+      document.querySelectorAll(".header-proposta")[0].style.color = "#000"
+      document.querySelectorAll(".header-proposta")[1].style.color = "#000"
+      document.querySelectorAll(".mensage-icon")[0].style.filter = "brightness(0%)"
+      document.querySelectorAll(".mensage-icon")[1].style.filter = "brightness(0%)"
+      document.querySelector(".softcom-logo").src = "assets/softcom-black.png"
+    }
+  });
