76418909a53192944487d5cc7e95768fc4857481
GuiJg <j.gu1lh3rm325@gmail.com>
2023-05-23 19:46:51 -0300

    Twelfth

--- script.js
+++ script.js
@@ -10,8 +10,10 @@
       document.querySelector(".softcom-logo").src = "assets/softcom-yellow.png"
       document.querySelectorAll(".header-proposta")[0].style.color = "#fff"
       document.querySelectorAll(".header-proposta")[1].style.color = "#fff"
+      document.querySelectorAll(".header-proposta")[2].style.color = "#fff"
       document.querySelectorAll(".icon-proposta")[0].style.filter = "none"
       document.querySelectorAll(".icon-proposta")[1].style.filter = "none"
+      document.querySelectorAll(".icon-proposta")[2].style.filter = "none"
     } else {
       menu.classList.remove("header-scroll");
       for (let i = 0; i < document.querySelectorAll(".header-link").length; i++) {
@@ -19,8 +21,10 @@
       }
       document.querySelectorAll(".header-proposta")[0].style.color = "#000"
       document.querySelectorAll(".header-proposta")[1].style.color = "#000"
+      document.querySelectorAll(".header-proposta")[2].style.color = "#000"
       document.querySelectorAll(".icon-proposta")[0].style.filter = "brightness(0%)"
       document.querySelectorAll(".icon-proposta")[1].style.filter = "brightness(0%)"
+      document.querySelectorAll(".icon-proposta")[2].style.filter = "brightness(0%)"
       document.querySelector(".softcom-logo").src = "assets/softcom-black.png"
     }
   });
